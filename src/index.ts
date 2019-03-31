184e0f18677ab460c63a407249d723c076c063b2
Nick Romito <nick@cribl.io>
2019-03-31 16:03:11 -0400

    Complete basic impl with retry

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,40 @@ process.on('SIGINT', () => {
 });
 
 if (process.argv.length < 4) {
-  console.log('Invalid args', process.argv);
+  console.log('usage: ', process.argv);
   process.exit(1);
 }
+initConnection();
 
-const socket = connect({host: process.argv[2], port: parseInt(process.argv[3])}, () => {
-  process.stdin.pipe(socket);
-});
-socket.on('error', err => {
-  console.error(err);
-});
+function initConnection() {
+  process.stdout.write('Connecting...');
+  retry().then((socket) => {
+    socket.on('close', () => initConnection());
+    process.stdout.write('\nConnected!\n');
+  });
+}
+
+function retry(): Promise<Socket> {
+  return connectTo(process.argv[2], parseInt(process.argv[3])).catch(err => {
+    if (err.code !== 'ECONNREFUSED') return Promise.resolve() as Promise<any>;
+    process.stdout.write('.');
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        retry().then(resolve).catch(reject);
+      }, 1000);
+    })
+  });
+}
+
+function connectTo(host: string, port: number): Promise<Socket> {
+  return new Promise((resolve, reject) => {
+    let socket = connect({host, port}, () => {
+      process.stdin.pipe(socket);
+      resolve(socket);
+    })
+    .on('error', err => {
+      socket.destroy();
+      reject(err);
+    })
+  })
+}
