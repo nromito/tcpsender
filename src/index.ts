504e6be1cbb910bcf9e0c20b03f36fe36b023d42
Nick Romito <nick@cribl.io>
2019-04-01 10:50:39 -0400

    Update to allow for stdin prompt config

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,52 @@
 import { Socket, connect } from "net";
 
+import * as readline from 'readline';
+
 process.on('SIGINT', () => {
   process.exit(0);
 });
-
+let p: Promise<any> = Promise.resolve();
 if (process.argv.length < 4) {
-  console.log('usage: ', process.argv);
-  process.exit(1);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  p = p.then(() => new Promise((resolve, reject) => {
+    rl.question('Host: ', answer => {
+      resolve(answer);
+    });
+  }))
+  .then((host) => new Promise((resolve, reject) => {
+    rl.question('Port: ', answer => {
+      resolve({host, port: parseInt(answer)});
+      rl.close();
+    });
+  }))
+} else {
+  p = p.then(() => {
+    const host = process.argv[2];
+    const port = parseInt(process.argv[3]);
+    return {host, port};
+  })
 }
-initConnection();
+p.then((hp) => initConnection(hp.host, hp.port));
 
-function initConnection() {
+
+function initConnection(host: string, port: number) {
   process.stdout.write('Connecting...');
-  retry().then((socket) => {
-    socket.on('close', () => initConnection());
+  retry(host, port).then((socket) => {
+    socket.on('close', () => initConnection(host, port));
     process.stdout.write('\nConnected!\n');
   });
 }
 
-function retry(): Promise<Socket> {
-  return connectTo(process.argv[2], parseInt(process.argv[3])).catch(err => {
+function retry(host: string, port: number): Promise<Socket> {
+  return connectTo(host, port).catch(err => {
     if (err.code !== 'ECONNREFUSED') return Promise.resolve() as Promise<any>;
     process.stdout.write('.');
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        retry().then(resolve).catch(reject);
+        retry(host, port).then(resolve).catch(reject);
       }, 1000);
     })
   });
